0edad66295b2e597fe5144ee291f22646e31bb49
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-01-08 10:37:57 +0100

    I added router to the category and Tag

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -2,10 +2,11 @@
 
 session_start();
 
-use App\Controllers\HomeController;
-use App\Controllers\UserController;
-
 use App\classes\Router;
+use App\Controllers\TagController;
+use App\Controllers\UserController;
+use App\Controllers\CategoryController;
+
 
 $router = new Router();
 
@@ -20,8 +21,15 @@ $router->post('/api/login', UserController::class, 'loginAction');
 $router->post('/api/register', UserController::class, 'registerAction');
 $router->get('/api/logout', UserController::class, 'logoutAction');
 
+$router->post('/api/category/create', CategoryController::class, 'createCtaegory');
+$router->get('/api/category/getall', CategoryController::class, 'getAllCtaegories');
+$router->delete('/api/category/deletcategory', CategoryController::class, 'deleteCtaegory');
+
+
+$router->post('/api/tag/create', TagController::class, 'createTag');
+$router->get('/api/tag/getall', TagController::class, 'getAllTags');
+$router->delete('/api/tag/deleteTag', TagController::class, 'deleteTag');
 
 
 
 $router->route();
-
