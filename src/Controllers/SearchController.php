8d4001f7805623979b7a293105d040618d166606
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-01-08 16:06:09 +0100

    i added SearchController to handle all type of searches in tha app

---
+++ src/Controllers/SearchController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controllers;
+
+use Exception;
+use App\Models\WikiModel;
+use App\Models\ImageModel;
+use App\classes\Controller;
+use App\classes\Validation;
+use App\Models\CategoryModel;
+use App\classes\ValidationException;
+use App\Models\TagModel;
+
+class SearchController extends Controller {
+
+    public function searchforTags(){
+        $param = $_GET['query'];
+
+        $tagModel = new TagModel();
+        $result =  $tagModel->searchforTag($param);
+
+        echo json_encode($result);
+
+    }
+
+    public function searchforWikies(){
+        $param = $_GET['query'];
+
+        $wikimodel = new WikiModel();
+        $result =  $wikimodel->searchforWiki($param);
+
+        echo json_encode($result);
+
+    }
+
+    public function getwikisbyTag(){
+
+        $tag = $_GET['tag'];
+
+        $tagModel = new TagModel();
+        $result =  $tagModel->selectWikiesByTag($tag);
+
+        echo json_encode($result);
+    }
+
+    public function getwikisbyCategory(){
+
+        $tag = $_GET['category'];
+
+        $categorymodel = new CategoryModel();
+        $result =  $categorymodel->selectWikiesByCategory($tag);
+
+        echo json_encode($result);
+    }
+
+}
