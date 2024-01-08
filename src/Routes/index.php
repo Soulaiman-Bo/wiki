789a942c6fa2d739fc9093a1effd94091bc8152d
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-01-08 14:17:03 +0100

    i added routes for the wikies

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -5,6 +5,7 @@ session_start();
 use App\classes\Router;
 use App\Controllers\TagController;
 use App\Controllers\UserController;
+use App\Controllers\WikiController;
 use App\Controllers\CategoryController;
 
 
@@ -21,6 +22,7 @@ $router->post('/api/login', UserController::class, 'loginAction');
 $router->post('/api/register', UserController::class, 'registerAction');
 $router->get('/api/logout', UserController::class, 'logoutAction');
 
+
 $router->post('/api/category/create', CategoryController::class, 'createCtaegory');
 $router->get('/api/category/getall', CategoryController::class, 'getAllCtaegories');
 $router->delete('/api/category/deletcategory', CategoryController::class, 'deleteCtaegory');
@@ -29,6 +31,13 @@ $router->delete('/api/category/deletcategory', CategoryController::class, 'delet
 $router->post('/api/tag/create', TagController::class, 'createTag');
 $router->get('/api/tag/getall', TagController::class, 'getAllTags');
 $router->delete('/api/tag/deleteTag', TagController::class, 'deleteTag');
+
+
+$router->post('/api/wiki/create', WikiController::class, 'createWiki');
+$router->get('/api/wiki/getall', WikiController::class, 'getAllWikies');
+$router->delete('/api/wiki/deleteWiki', WikiController::class, 'deleteWiki');
+
+
 
 
 
