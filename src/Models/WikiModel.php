dd71f710c2eb2157458d7f8a72230cde96a3d895
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-01-08 14:20:03 +0100

    i added InserWiki to the wikiModel to insert an article to the database

---
+++ src/Models/WikiModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+use PDOException;
+use App\classes\Model;
+
+
+class WikiModel extends Model
+{
+    public function InserWiki($data)
+    {
+        try {
+            $columns = implode(',', array_keys($data));
+            $values = implode(',', array_fill(0, count($data), '?'));
+
+            $sql = "INSERT INTO `article` ($columns) VALUES ($values)";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute(array_values($data));
+            $lastInsertId = $this->connection->lastInsertId();
+
+            return $lastInsertId;
+        } catch (PDOException $e) {
+            error_log("Database error: " . $e->getMessage() . "\n", 3, "errors.log");
+            echo "Database error: " . $e->getMessage();
+            return false;
+        }
+    }
+}
